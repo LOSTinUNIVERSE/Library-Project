70bc2405afcc39bb877fd646a3c1fb85a39e1a4f
abdulla <abdullaabdullaev0803@gmail.com>
2023-03-16 20:55:45 +0300

    add button to delete each books individually

--- index.js
+++ index.js
@@ -23,50 +23,67 @@ function checkRead() {
         readOrNot = "notread"
     }
 }
-
-const submitForm = document.querySelector("#showForm")
-submitForm.addEventListener("click", creatorOfBooks, false)
+const books = document.getElementsByClassName("books")
+function removeBook() {
+    let number = Number(this.value)
+    number -= 1
+    delete myLibrary[number]
+    myLibrary.length -= 1
+    books[number].remove()
+    console.log(myLibrary.length);
+    // console.log(books[1]);
+    // console.log(typeof myLibrary);
+}
+function ChangeLength() {
+    if (myLibrary.length == 0) {
+        myLibrary.length += 1
+    } else {
+        myLibrary.length -= 1
+    }
+}
 
 function showBooks() {
     const item = document.createElement("div")
     item.setAttribute("class", "books")
-    item.setAttribute("data-number", myLibrary.length)
+    item.setAttribute("datax-number", myLibrary.length)
     const bookTitle = document.createElement("h3")
     const bookAuthor = document.createElement("p")
     const bookPages = document.createElement("p")
     const deleteBook = document.createElement("button")
     deleteBook.innerText = "delete book"
+    deleteBook.setAttribute("data-number", myLibrary.length)
+    deleteBook.value = myLibrary.length
+    console.log("library length is ", `${myLibrary.length}`);
+    deleteBook.addEventListener("click", removeBook)
     table.appendChild(item)
     item.appendChild(bookTitle)
     item.appendChild(bookAuthor)
     item.appendChild(bookPages)
     item.appendChild(deleteBook)
-    console.log(myLibrary);
+    // console.log(myLibrary[0]);
 }
+// function empty() {
+//     if (author.value == "" || title.value == "" || pages.value == "") {
+//         return alert("did not enter")
+//     }
+// }
 function creatorOfBooks(event) {
     checkRead()
     const newBook = new Book(author.value, title.value,
         pages.value, readOrNot)
     myLibrary.push(newBook)
-    event.preventDefault()
-    console.log(newBook);
-    console.log(myLibrary);
     showBooks()
+    event.preventDefault()
 }
-function addBookToLibrary() {
-    myLibrary.push(input.value)
-}
-// const showBtn = document.querySelector("#showBooks")
-// showBtn.addEventListener("click", showBooks)
-// const addBook = document.getElementById("toAdd")
-// addBook.addEventListener("click", addBookToLibrary)
+function cleanTable() {
 
+}
+const submitForm = document.querySelector("#submitForm")
+submitForm.addEventListener("click", creatorOfBooks, false)
 
 function openForm() {
-    document.getElementById("myForm").style.display = "!none"
+    document.getElementById("myForm").style.display = "grid"
 }
 function closeForm() {
     document.getElementById("myForm").style.display = "none"
 }
-//
-// 
